f06ee7db8e31480942025d2895dbdcbfe65f6163
Armando <95893860+mandorc@users.noreply.github.com>
2025-09-20 20:44:28 -0600

    Code ready

--- BFS-GraphCheck/src/Main.java
+++ BFS-GraphCheck/src/Main.java
@@ -26,8 +26,84 @@
  *
  * @author armando
  */
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args){
+
+    static boolean isConnected(int[][] A) { // con BFS sobre matriz de adyacencia 
+        int n = A.length;   // obtemeos el tamano
+        if (n == 0) {       // comprobamos que no este vacio
+            return true;  
+        }
+        boolean[] visited = new boolean[n];   // vamos marcando los vertices vicitados
+        Deque<Integer> q = new ArrayDeque<>();// cola para BFS
+
+        visited[0] = true;             // comenzamos desde cero
+        q.add(0);         
+
+        // --- Bucle BFS ---
+        while (!q.isEmpty()) {
+            int v = q.removeFirst();   // se saca de la cola el siguiente vértice
+
+
+            for (int u = 0; u < n; u++) {
+
+                if (A[v][u] != 0 && !visited[u]) {
+                    visited[u] = true; // lo marcamos como visitado
+                    q.addLast(u);      
+                }
+            }
+        }
+
+        for (boolean b : visited) {
+            if (!b) {
+                return false;      
+            }
+        }
+        return true;     
+    }
+
+    public static void main(String[] args) {
+
+        // listamos nuestros ejemplos
+        int[][] ejemploA = {
+            {0, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 1},
+            {0, 0, 0, 1, 0}
+        };
+
+        int[][] ejemploB = {
+            {0, 1, 1, 0, 0},
+            {1, 0, 1, 0, 0},
+            {1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 1},
+            {0, 0, 0, 1, 0}
+        };
+
+        int[][] ejemploC = {
+            {0, 1, 1, 1, 0},
+            {1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0}
+        };
         
+        // este ejemplo fue visto en clase
+        int[][] ejemploD = {
+            {0,1,0,0,0,0,1},
+            {1,1,1,1,0,0,0},
+            {0,1,1,0,0,0,0},
+            {0,1,0,0,1,0,1},
+            {0,0,0,1,0,1,0},
+            {0,0,0,0,1,0,0},
+            {1,0,0,1,0,0,0}
+        };
+
+        System.out.println("Ejemplo A: " + (isConnected(ejemploA) ? "CONEXO" : "NO CONEXO"));
+        System.out.println("Ejemplo B: " + (isConnected(ejemploB) ? "CONEXO" : "NO CONEXO"));
+        System.out.println("Ejemplo C: " + (isConnected(ejemploC) ? "CONEXO" : "NO CONEXO"));
+        System.out.println("Ejemplo D: " + (isConnected(ejemploD) ? "CONEXO" : "NO CONEXO"));
     }
 }
